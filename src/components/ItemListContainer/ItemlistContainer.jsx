ae0fca1ede53e027f8a1c24b3cd3604f78257e59
Alejandro Luna <135073545+AlejandroLunaDev@users.noreply.github.com>
2024-03-20 19:26:16 -0300

    customHook useProductList

--- src/components/ItemListContainer/ItemlistContainer.jsx
+++ src/components/ItemListContainer/ItemlistContainer.jsx
@@ -1,38 +1,8 @@
-/* eslint-disable react/prop-types */
-import { useState, useEffect, useContext } from "react";
 import { ItemList } from "../ItemList/ItemList";
-import { getProduct } from "@/service/api/MercadoLibre";
-import { importProductsToFirebase } from "../../service/firebase/firebase";
-import { AppContext } from "@/context/AppContext";
-import { useParams } from "react-router-dom";
+import { useProductList } from '../../hook/useProductList.js';
 
 export function ItemlistContainer({ limit }) {
-  const [products, setProducts] = useState([]);
-  const { filterText } = useContext(AppContext);
-  const { category } = useParams();
-
-  useEffect(() => {
-    const fetchProducts = async () => {
-      try {
-        let searchQuery = category;
-
-        if (filterText) {
-          searchQuery = filterText;
-        }
-
-        const data = await getProduct(limit, searchQuery);
-        setProducts(data);
-
-        // Importar productos a Firebase
-        await importProductsToFirebase(limit, searchQuery);
-        console.log("Productos importados a Firebase.");
-      } catch (error) {
-        console.error("Error al obtener productos:", error.message);
-      }
-    };
-
-    fetchProducts();
-  }, [limit, filterText, category]);
+  const products = useProductList(limit);
 
   return (
     <div className="item-list-container">
